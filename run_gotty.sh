a6c27a2f581c0d5970d652a0346022c5d4289572
bxafnex1-blip <bxafnex1@gmail.com>
2026-06-17 23:56:55 +0600

    Update run_gotty.sh

--- run_gotty.sh
+++ run_gotty.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# Launches GoTTY and attaches to a persistent screen session.
-# If you close the browser, your bot keeps running!
-/usr/local/bin/gotty --permit-write --reconnect --title-format "Dev Terminal" /usr/bin/screen -xRR gotty_session
+
+/usr/local/bin/gotty --permit-write --reconnect /bin/bash
